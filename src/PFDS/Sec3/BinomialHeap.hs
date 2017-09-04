e89124ccfdcd0e217ec3312c8770a9f20e72e1cf
matonix <automatonix@gmail.com>
2017-09-05 01:29:41 +0900

    add 9

--- src/PFDS/Sec3/BinomialHeap.hs
+++ src/PFDS/Sec3/BinomialHeap.hs
@@ -1,60 +1,51 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-
 module PFDS.Sec3.BinomialHeap where
-{-# ANN module "HLint: ignore Eta reduce" #-}
 
-class Ord e => BinomialHeap h e where
-  empty :: [h e]
-  isEmpty :: [h e] -> Bool
-  rank :: h e -> Int
-  root :: h e -> e
-  link :: h e -> h e -> h e
-  insTree :: h e -> [h e] -> [h e]
-  insert :: e -> [h e] -> [h e]
-  merge :: [h e] -> [h e] -> [h e]
-  removeMinTree :: [h e] -> (h e, [h e])
-  findMin :: [h e] -> e
-  deleteMin :: [h e] -> [h e]
-
+import PFDS.Sec3.Heap (Heap (..))
 data Tree e = Node Int e [Tree e] deriving (Show)
+newtype BinomialHeap e = BH [Tree e] deriving (Show)
 
-instance Ord e => BinomialHeap Tree e where
-
-  empty = []
-  isEmpty = null
-
-  rank (Node r _ _) = r
-  root (Node _ x _) = x
-  link t1@(Node r x1 c1) t2@(Node _ x2 c2) =
-    if x1 <= x2
-      then Node (r+1) x1 (t2:c1)
-      else Node (r+1) x2 (t1:c2)
-
-  insTree t [] = [t]
-  insTree t ts@(t':ts') =
-    if rank t < rank t'
-      then t:ts
-      else insTree (link t t') ts'
-
-  insert x ts = insTree (Node 0 x []) ts
-
-  merge ts1 [] = ts1
-  merge [] ts2 = ts2
-  merge ts1@(t1:ts1') ts2@(t2:ts2')
-    | rank t1 < rank t2 = t1:merge ts1' ts2
-    | rank t2 < rank t1 = t2:merge ts1 ts2'
-    | otherwise = insTree (link t1 t2) (merge ts1' ts2')
-
-  removeMinTree [] = undefined
-  removeMinTree [t] = (t, [])
-  removeMinTree (t:ts) = let
-    (t', ts') = removeMinTree ts
-    in if root t <= root t'
-      then (t, ts)
-      else (t', t:ts')
-
-  findMin = root . fst . removeMinTree
-  deleteMin ts =
+instance Heap BinomialHeap where
+  empty = BH []
+  isEmpty (BH ts) = null ts
+  insert x (BH ts) = BH $ insTree (Node 0 x []) ts
+  merge (BH ts1) (BH ts2) = BH $ merge' ts1 ts2
+  findMin (BH ts) = root . fst . removeMinTree $ ts
+  deleteMin (BH ts) =
     let (Node _ _ ts1, ts2) = removeMinTree ts
-    in merge (reverse ts1) ts2
+    in BH $ merge' (reverse ts1) ts2
+
+rank :: Tree e -> Int
+rank (Node r _ _) = r
+
+root :: Tree e -> e
+root (Node _ x _) = x
+
+link :: Ord e => Tree e -> Tree e -> Tree e
+link t1@(Node r x1 c1) t2@(Node _ x2 c2) =
+  if x1 <= x2
+    then Node (r+1) x1 (t2:c1)
+    else Node (r+1) x2 (t1:c2)
+
+merge' :: Ord e => [Tree e] -> [Tree e] -> [Tree e]
+merge' ts1 [] = ts1
+merge' [] ts2 = ts2
+merge' ts1@(t1:ts1') ts2@(t2:ts2')
+  | rank t1 < rank t2 = t1:merge' ts1' ts2
+  | rank t2 < rank t1 = t2:merge' ts1 ts2'
+  | otherwise = insTree (link t1 t2) (merge' ts1' ts2')
+
+insTree :: Ord e => Tree e -> [Tree e] -> [Tree e]
+insTree t [] = [t]
+insTree t ts@(t':ts') =
+  if rank t < rank t'
+    then t:ts
+    else insTree (link t t') ts'
+
+removeMinTree :: Ord a => [Tree a] -> (Tree a, [Tree a])
+removeMinTree [] = undefined
+removeMinTree [t] = (t, [])
+removeMinTree (t:ts) = let
+  (t', ts') = removeMinTree ts
+  in if root t <= root t'
+    then (t, ts)
+    else (t', t:ts')
---
+++ src/PFDS/Sec5/Ex9.md
@@ -0,0 +1,63 @@
+# 演習問題 5.9
+
+> 二項ヒープ、スプレーヒープ、ペアリングヒープにおいて、償却上限が示すより長い時間がかかってしまう例を示す。
+
+### 償却上限の整理
+
+| 実／償却        | 二項ヒープ                   | スプレーヒープ            | ペアリングヒープ           |
+| ----------- | ----------------------- | ------------------ | ------------------ |
+| `insert`    | $O(\log n)$／$O(1)$      | $O(n)$／$O(\log n)$ | $O(1)$／$O(1)$      |
+| `merge`     | $O(\log n)$／$O(\log n)$ | $O(n)$／$O(\log n)$ | $O(1)$／$O(1)$      |
+| `deleteMin` | $O(\log n)$／$O(\log n)$ | $O(n)$／$O(\log n)$ | $O(n)$／$O(\log n)$ |
+
+### 方針
+
+- 実／償却コストのギャップが存在する操作を使って例を作る
+- 償却コストの埋め合わせ部分が発生する直前のデータを作る
+
+
+## 二項ヒープ
+
+`insert` が $O(1)$ 償却上限で動作することは 5.3 節で示されている。
+
+ここで、要素数 $n=2^k-1=\underbrace{(1...1)_2}_k$ の二項ヒープへ `insert` すると、$k$ 回 `link` されるため、`insert` の呼び出しは $k+1$ ステップすなわち $O(n)$ かかる。
+
+### 参考
+
+**before**
+
+![Ex9Binomial](C:\Users\maton\OneDrive\hs\pfds\pfds\src\PFDS\Sec5\Ex9Binomial.png)
+
+**after**
+
+![Ex9BinomialAfter](C:\Users\maton\OneDrive\hs\pfds\pfds\src\PFDS\Sec5\Ex9BinomialAfter.png)
+
+## スプレーヒープ
+
+`insert` が $O(\log n)$ 償却上限で動作することは 5.4 節で示されている。
+
+ここで、空ヒープに対して $n$ 個の値を昇順に `insert` したスプレーヒープを考えると、`partition` で全く木の回転が発生せず、深さ $n$ のヒープになる。
+
+このヒープに対し、先述のどの $n$ 個の値よりも小さい値を `insert` すると、最も深い位置まで `partition` が再帰するため $O(n)$ かかる。
+
+### 参考
+
+![Ex9Splay](C:\Users\maton\OneDrive\hs\pfds\pfds\src\PFDS\Sec5\Ex9Splay.png)
+
+## ペアリングヒープ
+
+`deleteMin` が $O(\log n)$ 償却上限で動作することは 5.5 節で示されている。
+
+ここで、空ヒープに対して $n$ 個の値を昇順に `insert` したペアリングヒープを考えると、`merge` で挿入される側の木に要素が増え続け、$n>1$ のとき根が $n-1$ 個の子のリストを持つヒープになる。
+
+このヒープに対し`deleteMin` すると、根が取れて、 $n-1$ 個の子に対し `mergePairs` が走査するため $O(n)$ かかる。
+
+### 参考
+
+**before**
+
+![Ex9Pairing](C:\Users\maton\OneDrive\hs\pfds\pfds\src\PFDS\Sec5\Ex9Pairing.png)
+
+**after**
+
+![Ex9PairingAfter](C:\Users\maton\OneDrive\hs\pfds\pfds\src\PFDS\Sec5\Ex9PairingAfter.png)
