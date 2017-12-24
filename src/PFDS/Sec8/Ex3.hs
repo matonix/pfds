9138ff2ac64dc29bb7babdd3c0467e1a12e1d12e
matonix <automatonix@gmail.com>
2017-12-24 16:13:13 +0900

    fix Ex3

--- src/PFDS/Sec8/Ex3.hs
+++ src/PFDS/Sec8/Ex3.hs
@@ -1,5 +1,7 @@
 module PFDS.Sec8.Ex3 where
 
+import Prelude hiding (head, tail)
+
 data RotationState a =
     Idle
   | Reversing Int [a] [a] [a] [a]
@@ -23,25 +25,24 @@ invalidate (Appending 0 f' (x:r')) = Done r'
 invalidate (Appending ok f' r') = Appending (ok-1) f' r'
 invalidate state = state
 
-exec1 :: Queue a -> Queue a
-exec1 (Q d f state r) = case exec state of
-  Done newf -> Q d newf Idle r
-  newstate@(Reversing ok _ _ _ _) -> Q (max d (ok*2+1)) f newstate r
-  newstate@(Appending ok _ _) -> Q (max d (ok*2+1)) f newstate r
-  newstate -> Q d f newstate r
+exec1 :: Int -> Queue a -> (Int, Queue a)
+exec1 l (Q d f state r) = case exec state of
+  Done newf -> (l', Q l' newf Idle r)
+  newstate -> (l', Q l' f newstate r)
+  where
+    l' = calcd l state
 
--- exec2 :: Queue a -> Queue a
--- exec2 (Q d f state r) = case exec (exec state) of
---   Done newf -> Q d newf Idle r
---   newstate@(Reversing ok _ _ _ _) -> Q (max d (ok*2+1)) f newstate r
---   newstate@(Appending ok _ _) -> Q (max d (ok*2+1)) f newstate r
---   newstate -> Q d f newstate r
+calcd :: Int -> RotationState a -> Int
+calcd l (Reversing _ (_:_) _ _ _) = l+2
+calcd l (Reversing _ [] _ _ _) = l+1
+calcd l _ = l
 
 check :: Queue a -> Queue a
 check q@(Q d f state r) = if d >= 0
-  then exec1 q
-  else exec1 $ exec1 (Q d f newstate [])
-  where newstate = Reversing 0 f [] r []
+  then snd $ exec1 d q
+  else snd . uncurry exec1 $ exec1 0 (Q 1 f newstate [])
+  where
+    newstate = Reversing 0 f [] r []
 
 empty :: Queue a
 empty = Q 0 [] Idle []
@@ -60,35 +61,3 @@ head (Q _ (x:_) _ _) = x
 tail :: Queue a -> Queue a
 tail (Q _ [] _ _) = error "empty"
 tail (Q d (x:f) state r) = check (Q (d-1) f (invalidate state) r)
-
--- *PFDS.Sec8.Ex3> mapM_ (print . foldl snoc empty) [[1..x] | x <- [1..30]]
--- Q 1 [1] Idle []
--- Q 0 [1] Idle [2]
--- Q 3 [1] (Appending 1 [1] [2,3]) []
--- Q 2 [1] (Appending 0 [] [1,2,3]) [4]
--- Q 1 [1,2,3] Idle [5,4]
--- Q 0 [1,2,3] Idle [6,5,4]
--- Q 5 [1,2,3] (Reversing 2 [3] [2,1] [5,4] [6,7]) []
--- Q 7 [1,2,3] (Reversing 3 [] [3,2,1] [4] [5,6,7]) [8]
--- Q 7 [1,2,3] (Appending 3 [3,2,1] [4,5,6,7]) [9,8]
--- Q 6 [1,2,3] (Appending 2 [2,1] [3,4,5,6,7]) [10,9,8]
--- Q 5 [1,2,3] (Appending 1 [1] [2,3,4,5,6,7]) [11,10,9,8]
--- Q 4 [1,2,3] (Appending 0 [] [1,2,3,4,5,6,7]) [12,11,10,9,8]
--- Q 3 [1,2,3,4,5,6,7] Idle [13,12,11,10,9,8]
--- Q 2 [1,2,3,4,5,6,7] Idle [14,13,12,11,10,9,8]
--- Q 1 [1,2,3,4,5,6,7] Idle [15,14,13,12,11,10,9,8]
--- Q 0 [1,2,3,4,5,6,7] Idle [16,15,14,13,12,11,10,9,8]
--- Q 5 [1,2,3,4,5,6,7] (Reversing 2 [3,4,5,6,7] [2,1] [15,14,13,12,11,10,9,8] [16,17]) []
--- Q 7 [1,2,3,4,5,6,7] (Reversing 3 [4,5,6,7] [3,2,1] [14,13,12,11,10,9,8] [15,16,17]) [18]
--- Q 9 [1,2,3,4,5,6,7] (Reversing 4 [5,6,7] [4,3,2,1] [13,12,11,10,9,8] [14,15,16,17]) [19,18]
--- Q 11 [1,2,3,4,5,6,7] (Reversing 5 [6,7] [5,4,3,2,1] [12,11,10,9,8] [13,14,15,16,17]) [20,19,18]
--- Q 13 [1,2,3,4,5,6,7] (Reversing 6 [7] [6,5,4,3,2,1] [11,10,9,8] [12,13,14,15,16,17]) [21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [23,22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [24,23,22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [25,24,23,22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [26,25,24,23,22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [27,26,25,24,23,22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [28,27,26,25,24,23,22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [29,28,27,26,25,24,23,22,21,20,19,18]
--- Q 15 [1,2,3,4,5,6,7] (Reversing 7 [] [7,6,5,4,3,2,1] [10,9,8] [11,12,13,14,15,16,17]) [30,29,28,27,26,25,24,23,22,21,20,19,18]
